43420316d1d99f1c14c7dad5bf201454b8065631
IOINITID <50173108+IOINITID@users.noreply.github.com>
2019-12-12 13:18:18 +0300

    Added block features

--- js/script.js
+++ js/script.js
@@ -1,11 +1,4 @@
-// var image = document.getElementsByClassName('thumbnail');
-// new simpleParallax(image, {
-// 	scale: 1.5
-// });
-
-// var rellax = new Rellax('.rellax');
-
 var rellax = new Rellax('.rellax', {
-  speed: 40,
+  speed: 35,
   vertical: true,
 });
