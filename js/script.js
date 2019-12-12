1b52a74b95c1cc2895f8a256c62f4d346f8fd492
IOINITID <50173108+IOINITID@users.noreply.github.com>
2019-12-12 11:47:01 +0300

    Added fonts, logo, styles and block header

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// var image = document.getElementsByClassName('thumbnail');
+// new simpleParallax(image, {
+// 	scale: 1.5
+// });
+
+// var rellax = new Rellax('.rellax');
+
+var rellax = new Rellax('.rellax', {
+  speed: 40,
+  vertical: true,
+});
