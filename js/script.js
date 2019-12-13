aa58841f2e073422671f047a80023aaefda4fdb8
IOINITID <50173108+IOINITID@users.noreply.github.com>
2019-12-13 17:29:05 +0300

    Added mobile version of project

--- js/script.js
+++ js/script.js
@@ -1,3 +1,44 @@
 var rellax = new Rellax('.rellax', {
   speed: 35
 });
+
+// slider-one element
+$('.slider-one').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  // autoplay: true,
+  autoplaySpeed: 5000,
+  arrows: false,
+  dots: false,
+  // prevArrow: '.slider-two-prev',
+  // nextArrow: '.slider-two-next',
+  responsive: [{
+    breakpoint: 1299,
+    settings: {
+      arrows: false,
+      slidesToShow: 1,
+      dots: true
+    }
+  }]
+});
+
+// slider-two element
+$('.slider-two').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  // autoplay: true,
+  autoplaySpeed: 5000,
+  arrows: false,
+  dots: false,
+  // prevArrow: '.slider-two-prev',
+  // nextArrow: '.slider-two-next',
+  responsive: [{
+    breakpoint: 1299,
+    settings: {
+      arrows: false,
+      slidesToShow: 1,
+      dots: true
+    }
+  }]
+});
+
